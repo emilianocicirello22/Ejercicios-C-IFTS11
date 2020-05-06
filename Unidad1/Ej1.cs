b769c3d9af78a6deabe37af8583cc2c3bd6f7957
Juan Emiliano Cicirello <juanemilianocicirello@outlook.com>
2020-05-05 23:00:37 -0300

    Ejercicio1

--- Unidad1/Ej1.cs
+++ Unidad1/Ej1.cs
@@ -9,9 +9,28 @@ namespace Ejercicio1
             //Declaracion de variables
             int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
 
+            //Pedimos e ingresamos primer numero
             Console.WriteLine("Ingrese un primer numero");
+           
+            n1 = int.Parse(Console.ReadLine());
+
+            //Pedimos e ingresamos segundo numero
+            Console.WriteLine("Ingrese un segundo numero");
+
+            n2 = int.Parse(Console.ReadLine());
+
+            //Pedimos e ingresamos tercero numero
+            Console.WriteLine("Ingrese un tercero numero");
+
+            n3 = int.Parse(Console.ReadLine());
+
+            //Pedimos e ingresamos cuarto numero
+            Console.WriteLine("Ingrese un cuarto numero");
+
+            n4 = int.Parse(Console.ReadLine());
             
-            Console.WriteLine("Hello World!");
+            //mostramos los numeros ingresados y realizamos la suma
+            Console.WriteLine($"La suma de {n1} + {n2} + {n3} + {n4} es: {n1+n2+n3+n4}");
         }
     }
 }
